6d1b6a5a5fffe52dc76460689a5142042d3bc4bb
Keeton Lanzinger <lanzing1@msu.edu>
2023-03-15 18:40:14 -0400

    added descriptions

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const supabaseUrl = 'https://afzhzgaeufygfizasysm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmemh6Z2FldWZ5Z2ZpemFzeXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MzA3OTUsImV4cCI6MTk5MzIwNjc5NX0.4BPg60lIeF2lM2CQ9ZfNnw0T5c1X86QP2KybKGUNgGI'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-let { data: books, error } = await supabase
-  .from('books')
-  .select('*')
+async function getBooks() {
+    let { data: books, error } = await supabase
+        .from('books')
+        .select('*')
 
-for (let book of books) {
-  let bookList = document.getElementById('books');
-  bookList.innerHTML += `<li>${book.title} – ${book.author} – ${book.ISBN}</li>`;
+    for (let book of books) {
+        let bookList = document.getElementById('books');
+        bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.description}</td></tr>`;
+    }
 }
+
+getBooks();
